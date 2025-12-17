522644747a2eab95b5f844d707dcc3a2f64a19aa
Jack Luo <30420522+thejackluo@users.noreply.github.com>
2025-12-17 01:36:06 -0500

    0.1: backend part 3

---
+++ weave-mobile/eslint.config.mjs
@@ -0,0 +1,40 @@
+import js from '@eslint/js';
+import typescript from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import prettier from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
+
+export default [
+  js.configs.recommended,
+  prettier,
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      '@typescript-eslint': typescript,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      '@typescript-eslint/no-unused-vars': 'warn',
+    },
+  },
+  {
+    ignores: [
+      'node_modules',
+      '.expo',
+      'dist',
+      'build',
+      '.vscode',
+    ],
+  },
+];
