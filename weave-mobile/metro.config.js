3e4524ced1e50190dcdc7393633c0ce9a3572e14
Jack Luo <30420522+thejackluo@users.noreply.github.com>
2025-12-17 21:14:02 -0500

    fixed: nativewind issue

--- weave-mobile/metro.config.js
+++ weave-mobile/metro.config.js
@@ -1,9 +1,8 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const { withNativeWind } = require('nativewind/metro');
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-// NativeWind v4 is configured via babel.config.js (nativewind/babel plugin)
-// No additional metro configuration needed
-
-module.exports = config;
+// NativeWind v4 Metro configuration
+module.exports = withNativeWind(config, { input: './global.css' });
