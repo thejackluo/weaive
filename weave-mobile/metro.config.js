44a74e278c85b90f9e5944923b1d623d088660ff
Jack Luo <30420522+thejackluo@users.noreply.github.com>
2025-12-21 12:10:39 -0500

    refactor: organized packages properly

--- weave-mobile/metro.config.js
+++ weave-mobile/metro.config.js
@@ -1,6 +1,18 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativewind } = require('nativewind/metro');
+const path = require('path');
 
 const config = getDefaultConfig(__dirname);
+
+// Configure for monorepo - watch design system package
+config.watchFolders = [
+  path.resolve(__dirname, '../packages/weave-design-system'),
+];
+
+// Configure node_modules resolution for monorepo
+config.resolver.nodeModulesPaths = [
+  path.resolve(__dirname, 'node_modules'),
+  path.resolve(__dirname, '../packages/weave-design-system/node_modules'),
+];
 
 module.exports = withNativewind(config, { input: './global.css' });
