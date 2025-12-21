c9c3d458ef4d0ebf8e6f288481a009323898d215
Jack Luo <30420522+thejackluo@users.noreply.github.com>
2025-12-20 23:59:37 -0500

    chore: jest and babel configs

--- packages/weave-design-system/.babelrc.js
+++ packages/weave-design-system/.babelrc.js
@@ -1,5 +1,11 @@
 module.exports = {
   presets: [
     '@react-native/babel-preset',
+    '@babel/preset-flow',
+    '@babel/preset-typescript',
+  ],
+  plugins: [
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
   ],
 };
--- packages/weave-design-system/jest.setup.js
+++ packages/weave-design-system/jest.setup.js
@@ -1,19 +1,72 @@
 // Mock react-native-reanimated
 jest.mock('react-native-reanimated', () => {
-  const Reanimated = require('react-native-reanimated/mock');
+  // Create a comprehensive manual mock without relying on react-native
+  const React = require('react');
 
-  // Mock the worklet directive
-  Reanimated.default.call = () => {};
+  // Simple mock component
+  const MockComponent = (props) => React.createElement('View', props, props.children);
 
-  // Mock animation functions
-  Reanimated.withSpring = jest.fn((toValue, config) => toValue);
-  Reanimated.withTiming = jest.fn((toValue, config) => toValue);
-
-  return Reanimated;
+  return {
+    __esModule: true,
+    default: {
+      View: MockComponent,
+      createAnimatedComponent: (component) => component,
+      call: jest.fn(),
+    },
+    // Animation functions
+    withSpring: jest.fn((toValue, config) => toValue),
+    withTiming: jest.fn((toValue, config) => toValue),
+    withDecay: jest.fn((config) => 0),
+    withSequence: jest.fn((...animations) => animations[0]),
+    withRepeat: jest.fn((animation, count) => animation),
+    withDelay: jest.fn((delay, animation) => animation),
+    // Hooks
+    useSharedValue: jest.fn((initial) => ({ value: initial })),
+    useAnimatedStyle: jest.fn((callback) => callback()),
+    useAnimatedProps: jest.fn((callback) => callback()),
+    useAnimatedGestureHandler: jest.fn(() => ({})),
+    useAnimatedScrollHandler: jest.fn(() => ({})),
+    useAnimatedReaction: jest.fn(),
+    useDerivedValue: jest.fn((callback) => ({ value: callback() })),
+    useAnimatedRef: jest.fn(() => ({ current: null })),
+    // Utilities
+    runOnJS: jest.fn((fn) => fn),
+    runOnUI: jest.fn((fn) => fn),
+    cancelAnimation: jest.fn(),
+    measure: jest.fn(),
+    scrollTo: jest.fn(),
+    // Easing
+    Easing: {
+      linear: jest.fn((t) => t),
+      ease: jest.fn((t) => t),
+      quad: jest.fn((t) => t * t),
+      cubic: jest.fn((t) => t * t * t),
+      bezier: jest.fn(() => (t) => t),
+      in: jest.fn((easing) => easing),
+      out: jest.fn((easing) => easing),
+      inOut: jest.fn((easing) => easing),
+    },
+    // Animated components
+    View: MockComponent,
+    Text: MockComponent,
+    Image: MockComponent,
+    ScrollView: MockComponent,
+  };
 });
 
-// Mock react-native modules
-jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+// Mock react-native AccessibilityInfo
+jest.mock('react-native', () => ({
+  AccessibilityInfo: {
+    isReduceMotionEnabled: jest.fn(() => Promise.resolve(false)),
+    addEventListener: jest.fn(() => ({
+      remove: jest.fn(),
+    })),
+  },
+  Platform: {
+    OS: 'ios',
+    select: jest.fn((obj) => obj.ios || obj.default),
+  },
+}));
 
 // Silence console warnings in tests
 global.console = {
