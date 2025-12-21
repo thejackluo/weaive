9aabe31c401eb9c4a7c6422bc309976d734ba336
Jack Luo <30420522+thejackluo@users.noreply.github.com>
2025-12-21 16:03:27 -0500

    ds1.1 new: chore tamagui configs

--- packages/weave-design-system/.babelrc.js
+++ packages/weave-design-system/.babelrc.js
@@ -7,5 +7,14 @@ module.exports = {
   plugins: [
     ['@babel/plugin-proposal-decorators', { legacy: true }],
     ['@babel/plugin-proposal-class-properties', { loose: true }],
+    [
+      '@tamagui/babel-plugin',
+      {
+        components: ['tamagui'],
+        config: './src/tamagui.config.ts',
+        logTimings: true,
+        disableExtraction: process.env.NODE_ENV === 'development',
+      },
+    ],
   ],
 };
